d9e43370f8bcde23c6f4de0cdaa53cf05cad1997
dhruv50ae <linkdp2012@gmail.com>
2022-07-07 00:27:56 +0100

    Set up final project file

--- AnotherMod.py
+++
@@ -1,1 +0,0 @@
-anotherVar = 2
--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-# from turtle import Turtle, Screen
-# import AnotherMod
-#
-# print(AnotherMod.anotherVar)
-#
-# rammus = Turtle()
-# print(rammus)
-# rammus.shape("turtle")
-# rammus.color("coral")
-# rammus.forward(100)
-# rammus.backward(200)
-#
-# myScreen = Screen()
-#
-# myScreen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from prettytable import PrettyTable
-
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-print(table)
