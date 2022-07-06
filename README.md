c07b79bfe41d19b7b106dadabfeb511d9d3ea800
dhruv50ae <linkdp2012@gmail.com>
2022-07-07 00:16:26 +0100

    Use Pretty Table

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# pObjectOP
 # pCoffeeMachineOOP
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from turtle import Turtle, Screen
-import AnotherMod
+# from turtle import Turtle, Screen
+# import AnotherMod
+#
+# print(AnotherMod.anotherVar)
+#
+# rammus = Turtle()
+# print(rammus)
+# rammus.shape("turtle")
+# rammus.color("coral")
+# rammus.forward(100)
+# rammus.backward(200)
+#
+# myScreen = Screen()
+#
+# myScreen.exitonclick()
 
-print(AnotherMod.anotherVar)
+from prettytable import PrettyTable
 
-rammus = Turtle()
-print(rammus)
-rammus.shape("turtle")
-rammus.color("coral")
-rammus.forward(100)
-rammus.backward(200)
-
-myScreen = Screen()
-
-myScreen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
