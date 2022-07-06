c90d9fd55b72b0d839f63a3029cf0ddb404de631
dhruv50ae <linkdp2012@gmail.com>
2022-07-06 15:23:06 +0100

    Turtle Library
    You can import turtle which is built into python and use it to create a turtle on screen. Turtle also has Screen which means you can create a graphical way of presenting what is going on when giving the turtle commands

---
+++ AnotherMod.py
@@ -0,0 +1,1 @@
+anotherVar = 2
---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+import AnotherMod
+
+print(AnotherMod.anotherVar)
+
+rammus = Turtle()
+print(rammus)
+rammus.shape("turtle")
+rammus.color("coral")
+rammus.forward(100)
+rammus.backward(200)
+
+myScreen = Screen()
+
+myScreen.exitonclick()
